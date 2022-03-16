d027086d085abbcf22adf277319b879e2e1791e8
Your Name <you@example.com>
2022-03-16 17:50:24 -0300

    verificacion del formulario

--- js/app.js
+++ js/app.js
@@ -1,4 +1,62 @@
-const btnEnviar = document.querySelector('.boton--primario');
+const datos = {
+    nombre : '',
+    email: '',
+    mensaje: ''
+}
 
-console.log(btnEnviar);
+const nombre = document.querySelector('#nombre');
+const email = document.querySelector('#email');
+const mensaje = document.querySelector('#mensaje');
+const formulario = document.querySelector('.formularioContacto');
+
+
+nombre.addEventListener('input', leerTexto);
+email.addEventListener('input', leerTexto);
+mensaje.addEventListener('input', leerTexto);
+
+function leerTexto(e) {
+
+    datos[e.target.id] = e.target.value;   
+}
+
+
+formulario.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const {nombre, email, mensaje} = datos;
+    console.log(datos);
+    if(nombre === '' || email === '' || mensaje === '' ) {
+        console.log('Al menos un campo esta vacio');
+        mostrarError('Todos los campos son obligatorios');
+        return; 
+    }
+
+    console.log('SE MANDA')
+
+    mostrarMensaje('Mensaje enviado correctamente');
+
+});
+
+
+function mostrarError(mensaje) {
+    const alerta = document.createElement('p');
+    alerta.textContent = mensaje;
+    alerta.classList.add('error');
+
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
+
+function mostrarMensaje(mensaje) {
+    const alerta = document.createElement('p');
+    alerta.textContent = mensaje;
+    alerta.classList.add('correcto');
+    formulario.appendChild(alerta);
+
+    setTimeout(() => {
+        alerta.remove();
+    }, 3000);
+}
 
